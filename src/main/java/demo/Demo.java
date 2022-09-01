499e680271aa13d1371ec229d471801f56057995
Oleksii Demchenko <demchenko.alexey.v@gmail.com>
2022-09-01 19:20:01 +0300

    Add basic Session Factory implementation and docker-compose file

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,27 @@
+package demo;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import demo.entity.Person;
+import learnorm.session.SessionFactory;
+import learnorm.session.SessionFactoryImpl;
+
+import javax.sql.DataSource;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new SessionFactoryImpl(initializeDataSource());
+        var session = sessionFactory.openSession();
+
+        var person = session.find(Person.class, 1L);
+        System.out.println(person);
+    }
+
+    private static DataSource initializeDataSource() {
+        var dataSource = new MysqlDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/person");
+        dataSource.setUser("admin");
+        dataSource.setPassword("admin");
+        return dataSource;
+    }
+}
